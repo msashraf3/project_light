2ca68568a107be47d1cc7b03f76eb65da198c966
msashraf3 <msashrafsingdho@gmail.com>
2023-04-28 09:45:37 +0600

    testing buttons

---
+++ main js.js
@@ -0,0 +1,39 @@
+function takeScreenshot() {
+    // Hide the buttons
+    document.getElementById("buttons").style.display = "none";
+  
+    // Set the size of the canvas to the size of the window
+    var canvas = document.createElement("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  
+    // Get the context of the canvas
+    var ctx = canvas.getContext("2d");
+  
+    // Scroll to the top of the page and draw the entire page onto the canvas
+    window.scrollTo(0, 0);
+    ctx.drawImage(document.body, 0, 0, window.innerWidth, window.innerHeight);
+  
+    // Append the canvas to a new div
+    var screenshotDiv = document.getElementById("screenshot-div");
+    screenshotDiv.appendChild(canvas);
+    screenshotDiv.style.display = "block";
+  
+    // Create a data URL from the canvas and set it as the href of the download link
+    var dataURL = canvas.toDataURL("image/png");
+    document.getElementById("download-link").href = dataURL;
+    document.getElementById("download-link").style.display = "block";
+  
+    // Show the buttons again after a delay
+    setTimeout(function() {
+      document.getElementById("buttons").style.display = "block";
+      screenshotDiv.innerHTML = '';
+      document.getElementById("download-link").style.display = "none";
+    }, 2000);
+  }
+  
+  function hideButtons() {
+    // Hide the buttons
+    document.getElementById("buttons").style.display = "none";
+  }
+  
